6ce9b3ee916ea938643ec227c82a4aebbe48f15b
Haokun Tian <tianhaokun258@gmail.com>
2023-11-19 14:53:24 +0000

    update 8-fold CV

--- code/average_8foldCV_results.py
+++ code/average_8foldCV_results.py
@@ -8,10 +8,8 @@ if __name__ == '__main__':
 
     exp_dir = os.path.join(f'{DATA_PATH}/../exp', sys.argv[1])
     for dataset in TRAIN_DATASET:
-        beat1 = []
-        beat2 = []
-        downbeat1 = []
-        downbeat2 = []
+        beat_fmeasure, beat_cmlt, beat_amlt = [],[],[]
+        downbeat_fmeasure, downbeat_cmlt, downbeat_amlt = [],[],[]
         for k in range(8):
             fold_path = os.path.join(exp_dir, f'fold{k}')
             if not os.path.isdir(fold_path):
@@ -22,14 +20,14 @@ if __name__ == '__main__':
                 for i, line in enumerate(lines):
                     line = re.split(' ', line.strip())
                     if i == 3:
-                        beat1.append(float(line[1]))
-                    elif i == 5:
-                        beat2.append(float(line[1]))
-                    elif i == 9:
-                        downbeat1.append(float(line[1]))
+                        beat_fmeasure.append(float(line[1]))
+                        beat_cmlt.append(float(line[11]))
+                        beat_amlt.append(float(line[15]))
                     elif i == 11:
-                        downbeat2.append(float(line[1]))
+                        downbeat_fmeasure.append(float(line[1]))
+                        downbeat_cmlt.append(float(line[11]))
+                        downbeat_amlt.append(float(line[15]))
         print(f'{dataset}:')
-        print(f'beat: {np.mean(beat1):.3f}, {np.mean(beat2):.3f}')
+        print(f'beat: f_measure {np.mean(beat_fmeasure):.3f} CMLt {np.mean(beat_cmlt):.3f} AMLt {np.mean(beat_amlt):.3f}')
         if dataset != 'smc':
-            print(f'downbeat: {np.mean(downbeat1):.3f}, {np.mean(downbeat2):.3f}')
+            print(f'downbeat: f_measure {np.mean(downbeat_fmeasure):.3f} CMLt {np.mean(downbeat_cmlt):.3f} AMLt {np.mean(downbeat_amlt):.3f}')
